ea355451bbc983d89d0a368fa8d9af28e6096856
Gizmo <nalanbilgin@gmail.com>
2024-07-03 13:43:21 +0300

    Added more components
    Added more components with unimplemented functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Gallery from "./pages/Gallery";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route index element={<Homepage />}></Route>
         <Route path="/gallery" element={<Gallery />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/app" element={<AppLayout />}></Route>
       </Routes>
     </BrowserRouter>
   );
