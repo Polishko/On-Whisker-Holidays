010b95bc829cfd4f4eeb520072930b1dbbe713ba
Gizmo <nalanbilgin@gmail.com>
2024-07-11 19:34:50 +0300

    Implemented user registration
    Implemented user registration logic

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,48 +1,109 @@
+import { useState } from "react";
 import Button from "../components/Common/Button";
 import PageNav from "../components/PageNav";
 import styles from "./Registration.module.css";
+import { useUsers } from "../components/contexts/UsersContext";
+import AvatarSelection from "../components/Common/AvatarSelection";
 
-function Registrtion() {
-  function handleSubmit(e) {
+function Registration() {
+  const avatars = [
+    { id: "cat1", src: "/avatar/cat1.png" },
+    { id: "cat2", src: "/avatar/cat2.png" },
+    { id: "cat3", src: "/avatar/cat3.png" },
+    { id: "dog1", src: "/avatar/dog1.png" },
+    { id: "dog2", src: "/avatar/dog2.png" },
+    { id: "dog3", src: "/avatar/dog3.png" },
+  ];
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [selectedAvatar, setSelectedAvatar] = useState("cat1");
+  const { createUser } = useUsers();
+
+  async function handleRegister(e) {
     e.preventDefault();
+    await createUser({
+      name,
+      email,
+      password,
+      avatar: `../public/avatar/${selectedAvatar}.png`,
+    });
+    alert("User registered successfully");
+  }
+
+  function handleAvatarChange(e) {
+    setSelectedAvatar(e.target.value);
   }
 
   return (
     <main className={styles.registration}>
       <PageNav />
 
-      <form
-        className={`${styles.form} ${styles["registration-form"]}`}
-        onSubmit={handleSubmit}
-      >
-        <div className={styles["registration-header"]}>
-          <header>Register here</header>
+      <form className={`${styles.form}`} onSubmit={handleRegister}>
+        <header>Register here</header>
+
+        <div className={styles.row}>
+          <label htmlFor="name" name="name">
+            Enter username
+          </label>
+          <div className={styles.inputContainer}>
+            <i className={`bx bx-user ${styles.icon}`}></i>
+            <input
+              type="text"
+              id="name"
+              placeholder="Username"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
         </div>
+
         <div className={styles.row}>
           <label htmlFor="email" name="email">
             Enter your email
           </label>
           <div className={styles.inputContainer}>
-            <i className={`bx bx-user ${styles.icon}`}></i>
-            <input type="email" id="email" placeholder="Username" />
+            <i className={`bx bx-envelope ${styles.icon}`}></i>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <label htmlFor="password" name="password">
-            Enter your password
+            Enter a strong password
           </label>
           <div className={styles.inputContainer}>
             <i className={`bx bx-lock ${styles.icon}`}></i>
-            <input type="password" id="password" placeholder="Password" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
         </div>
+
+        <div className={styles.row}></div>
 
         <div className={styles.bottom}>
           <div className={styles.left}>
             <p>Choose avatar</p>
+            <AvatarSelection
+              avatars={avatars}
+              selectedAvatar={selectedAvatar}
+              handleAvatarChange={handleAvatarChange}
+            />
           </div>
         </div>
+
         <Button type={"submit"} className={styles["button-style"]}>
           Register
         </Button>
@@ -51,4 +112,4 @@ function Registrtion() {
   );
 }
 
-export default Registrtion;
+export default Registration;
