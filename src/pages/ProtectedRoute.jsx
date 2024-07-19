6d1b70f1c1ed3a01cd597b8765fd6de9b7364d6e
Gizmo <nalanbilgin@gmail.com>
2024-07-19 18:23:43 +0300

    removed unnecessary outlet

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,15 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../components/contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
-function ProtectedRoute() {
+function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
-    // Redirect to the login page or home page if the user is not authenticated
     return <Navigate to="/login" />;
   }
-
-  return <Outlet />;
+  return children;
 }
 
 export default ProtectedRoute;
