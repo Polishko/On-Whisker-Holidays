356d1db8ce8db1dd315bd3706d549fd1916bd65e
Gizmo <nalanbilgin@gmail.com>
2024-07-19 18:25:04 +0300

    path corrected

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../components/contexts/AuthContext";
 
 function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuth();
