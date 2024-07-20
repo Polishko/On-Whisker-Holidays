601fd889ef7d5ea68f1d7862c67cfc3ec0e8822b
Gizmo <nalanbilgin@gmail.com>
2024-07-20 22:51:13 +0300

    removed redundant

--- src/components/commoncomponents/SpecificsEmojis.jsx
+++
@@ -1,7 +0,0 @@
-import { specificsToEmoji } from "../../helpers/specificsToEmoji";
-
-function SpecificsEmojis({ specifics }) {
-  return <span>{specificsToEmoji(specifics)}</span>;
-}
-
-export default SpecificsEmojis;
