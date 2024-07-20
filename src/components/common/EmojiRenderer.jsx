50f0ed27812e8b9513870594476887c5cb3b7191
Gizmo <nalanbilgin@gmail.com>
2024-07-20 22:49:38 +0300

    removed to rename

--- src/components/common/EmojiRenderer.jsx
+++
@@ -1,8 +0,0 @@
-import { flagemojiToPNG } from "../../helpers/flagemojiToPNG";
-
-function EmojiRenderer({ emoji }) {
-  const src = flagemojiToPNG(emoji);
-  return <img src={src} alt="flag" />;
-}
-
-export default EmojiRenderer;
