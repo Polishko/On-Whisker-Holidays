9c2e1b91d8326d415c920731c6a5a86119d766da
Gizmo <nalanbilgin@gmail.com>
2024-07-26 17:59:22 +0300

    Added my hotel locations to map

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -5,8 +5,12 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useState } from "react";
 
+import { useHotels } from "../components/contexts/HotelsContext";
+
 function Map() {
   const navigate = useNavigate();
+  const { hotels } = useHotels();
+
   const [mapPosition, setMapPosition] = useState([
     42.70540597995496, 23.328738252150977,
   ]);
@@ -20,18 +24,23 @@ function Map() {
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {hotels.map((hotel) => (
+          <Marker
+            position={[hotel.position.lat, hotel.position.lng]}
+            key={hotel.id}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
