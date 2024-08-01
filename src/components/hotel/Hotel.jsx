b16a75ddee85e0b1ebc41c5df15113f42ab46f08
Gizmo <nalanbilgin@gmail.com>
2024-08-01 12:18:44 +0300

    replaced alerts with modal in hotel add comment functionality

--- src/components/hotel/Hotel.jsx
+++ src/components/hotel/Hotel.jsx
@@ -1,3 +1,189 @@
+// import { useNavigate, useParams } from "react-router-dom";
+// import { useEffect, useState } from "react";
+
+// import styles from "./Hotel.module.css";
+
+// import { useAuth } from "../contexts/AuthContext";
+// import { useComments } from "../contexts/CommentsContext";
+// import { useHotels } from "../contexts/HotelsContext";
+// import { useCheckAuth } from "../../hooks/useCheckTokenValidity";
+// import { useKey } from "../../hooks/useKey";
+// import { useModal } from "../../hooks/useModal";
+
+// import Spinner from "../common/Spinner";
+// import EmojiRenderer from "../common/EmojiRenderer";
+// import Facilities from "../common/Facilities";
+// import Message from "../common/Message";
+// import Button from "../common/Button";
+// import Weather from "../common/Weather";
+// import CommentModal from "../modal/CommentModal";
+
+// function Hotel() {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+
+//   const { getHotel, currentHotel, isLoading } = useHotels();
+//   const { isAuthenticated } = useAuth();
+//   const { createComment, fetchComments } = useComments();
+//   const checkAuth = useCheckAuth();
+
+//   const { isModalOpen, modalMessage, openModal, closeModal } = useModal();
+
+//   // const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [comment, setComment] = useState("");
+//   const [charCount, setCharCount] = useState(0);
+
+//   useEffect(() => {
+//     if (id) getHotel(id);
+//   }, [id, getHotel]);
+
+//   // useEffect(() => {
+//   //   return () => {
+//   //     setIsModalOpen(false);
+//   //   };
+//   // }, [navigate]);
+
+//   const {
+//     hotelName,
+//     city,
+//     detail,
+//     img,
+//     web,
+//     countryCode: emoji,
+//     position: { lat: latitude, lng: longitude },
+//   } = currentHotel;
+
+//   function handleAddComment() {
+//     if (!checkAuth()) return;
+//     openModal();
+//   }
+
+//   function handleCharChange(e) {
+//     const value = e.target.value;
+//     if (value.length <= 80) {
+//       setComment(value);
+//       setCharCount(value.length);
+//     }
+//   }
+
+//   async function handleCommentSubmit(e) {
+//     e.preventDefault();
+//     if (!comment) {
+//       alert("Please add a comment to submit!");
+//       return;
+//     }
+//     try {
+//       const result = await createComment(comment, id);
+//       if (result.success) {
+//         handleCloseModal();
+//         alert("Comment added successfully.");
+//         await fetchComments();
+//       } else {
+//         console.error(result.message);
+//       }
+//     } catch (error) {
+//       alert("There was an error adding the comment.");
+//     }
+//   }
+
+//   function handleCloseModal() {
+//     setIsModalOpen(false);
+//     setComment("");
+//     setCharCount(0);
+//   }
+
+//   useKey("Escape", (e) => {
+//     if (isModalOpen) {
+//       handleCloseModal(e);
+//     }
+//   });
+
+//   useKey("Enter", (e) => {
+//     if (isModalOpen) {
+//       handleCommentSubmit(e);
+//     }
+//   });
+
+//   if (isLoading) return <Spinner />;
+
+//   if (currentHotel.hotelName === "")
+//     return (
+//       <div className={styles.details}>
+//         <Message
+//           message={"Click to a hotel to find out more!"}
+//           background={"light"}
+//         />
+//       </div>
+//     );
+
+//   return (
+//     <div className={styles.hotelCard}>
+//       <section className={styles.left}>
+//         <div className={styles.imageContainer}>
+//           <img src={`../../${img}`} alt="hotel" />
+//         </div>
+//       </section>
+
+//       <section className={styles.right}>
+//         <h3 className={styles.hotelTitle}>
+//           <p className={styles.hotelName}>{hotelName}</p>
+//         </h3>
+
+//         <div className={styles.location}>
+//           <p className={styles.city}>📍 {city}</p>
+//           <span>
+//             <EmojiRenderer emoji={emoji} />
+//           </span>
+//           <div className={styles.weather}>
+//             <Weather latitude={latitude} longitude={longitude} />
+//           </div>
+//         </div>
+
+//         <div>
+//           <div className={styles.aveRating}>ave rating</div>
+//           <div className={styles.rate}>rate</div>
+//         </div>
+
+//         <div className={styles.detail}>
+//           <p>{detail}</p>
+//         </div>
+
+//         <div className={styles.moreInfo}>
+//           <p className={styles.webAddress}>
+//             <strong>Website:</strong>{" "}
+//             <a href={web} target="_blank" rel="noopener noreferrer">
+//               {web}
+//             </a>
+//           </p>
+//           <div className={styles.bottom}>
+//             <Facilities hotel={currentHotel} />
+//             {isAuthenticated && (
+//               <Button
+//                 className={styles.addComment}
+//                 type="quaternary"
+//                 onClick={handleAddComment}
+//               >
+//                 Add comment
+//               </Button>
+//             )}
+//           </div>
+//         </div>
+//       </section>
+
+//       {isModalOpen && (
+//         <CommentModal
+//           handleCloseModal={handleCloseModal}
+//           handleCharChange={handleCharChange}
+//           handleCommentSubmit={handleCommentSubmit}
+//           comment={comment}
+//           charCount={charCount}
+//         />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Hotel;
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
@@ -6,8 +192,9 @@ import styles from "./Hotel.module.css";
 import { useAuth } from "../contexts/AuthContext";
 import { useComments } from "../contexts/CommentsContext";
 import { useHotels } from "../contexts/HotelsContext";
-import { useKey } from "../../hooks/useKey";
 import { useCheckAuth } from "../../hooks/useCheckTokenValidity";
+import { useKey } from "../../hooks/useKey";
+import { useModal } from "../../hooks/useModal";
 
 import Spinner from "../common/Spinner";
 import EmojiRenderer from "../common/EmojiRenderer";
@@ -16,32 +203,24 @@ import Message from "../common/Message";
 import Button from "../common/Button";
 import Weather from "../common/Weather";
 import CommentModal from "../modal/CommentModal";
+import Modal from "../modal/Modal";
 
 function Hotel() {
   const { id } = useParams();
-  const navigate = useNavigate();
-
   const { getHotel, currentHotel, isLoading } = useHotels();
   const { isAuthenticated } = useAuth();
   const { createComment, fetchComments } = useComments();
   const checkAuth = useCheckAuth();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { isModalOpen, modalMessage, openModal, closeModal } = useModal();
+
   const [comment, setComment] = useState("");
   const [charCount, setCharCount] = useState(0);
-
-  useEffect(
-    function () {
-      if (id) getHotel(id);
-    },
-    [id, getHotel]
-  );
+  const [modalType, setModalType] = useState(""); // NEW STATE
 
   useEffect(() => {
-    return () => {
-      setIsModalOpen(false);
-    };
-  }, [navigate]);
+    if (id) getHotel(id);
+  }, [id, getHotel]);
 
   const {
     hotelName,
@@ -55,7 +234,8 @@ function Hotel() {
 
   function handleAddComment() {
     if (!checkAuth()) return;
-    setIsModalOpen(true);
+    setModalType("comment"); // Set modal type to comment
+    openModal();
   }
 
   function handleCharChange(e) {
@@ -69,27 +249,31 @@ function Hotel() {
   async function handleCommentSubmit(e) {
     e.preventDefault();
     if (!comment) {
-      alert("Please add a comment to submit!");
+      setModalType("message"); // Set modal type to message
+      openModal("Please add a comment to submit!");
       return;
     }
     try {
       const result = await createComment(comment, id);
       if (result.success) {
-        handleCloseModal();
-        alert("Comment added successfully.");
+        setModalType("message"); // Set modal type to message
+        openModal("Comment added successfully.");
         await fetchComments();
       } else {
-        console.error(result.message);
+        setModalType("message"); // Set modal type to message
+        openModal(result.message);
       }
     } catch (error) {
-      alert("There was an error adding the comment.");
+      setModalType("message"); // Set modal type to message
+      openModal("There was an error adding the comment.");
     }
   }
 
   function handleCloseModal() {
-    setIsModalOpen(false);
+    closeModal();
     setComment("");
     setCharCount(0);
+    setModalType(""); // Reset modal type
   }
 
   useKey("Escape", (e) => {
@@ -99,8 +283,10 @@ function Hotel() {
   });
 
   useKey("Enter", (e) => {
-    if (isModalOpen) {
+    if (isModalOpen && modalType === "comment") {
       handleCommentSubmit(e);
+    } else if (isModalOpen) {
+      handleCloseModal();
     }
   });
 
@@ -110,7 +296,7 @@ function Hotel() {
     return (
       <div className={styles.details}>
         <Message
-          message={"Click to a hotel to find out more!"}
+          message={"Click a hotel to find out more!"}
           background={"light"}
         />
       </div>
@@ -171,13 +357,19 @@ function Hotel() {
       </section>
 
       {isModalOpen && (
-        <CommentModal
-          handleCloseModal={handleCloseModal}
-          handleCharChange={handleCharChange}
-          handleCommentSubmit={handleCommentSubmit}
-          comment={comment}
-          charCount={charCount}
-        />
+        <Modal onClose={handleCloseModal}>
+          {modalType === "message" ? (
+            <p>{modalMessage}</p>
+          ) : (
+            <CommentModal
+              handleCloseModal={handleCloseModal}
+              handleCharChange={handleCharChange}
+              handleCommentSubmit={handleCommentSubmit}
+              comment={comment}
+              charCount={charCount}
+            />
+          )}
+        </Modal>
       )}
     </div>
   );
--- src/components/modal/CommentModal.jsx
+++ src/components/modal/CommentModal.jsx
@@ -1,5 +1,35 @@
+// import Button from "../common/Button";
+// import Modal from "./Modal";
+
+// function CommentModal({
+//   handleCloseModal,
+//   handleCharChange,
+//   handleCommentSubmit,
+//   comment,
+//   charCount,
+// }) {
+//   return (
+//     <Modal onClose={handleCloseModal}>
+//       <div>
+//         <textarea
+//           value={comment}
+//           onChange={handleCharChange}
+//           maxLength="80"
+//           placeholder="Write your comment here..."
+//         />
+//         <p>{charCount}/80 characters</p>
+//         <Button type="secondary" onClick={handleCommentSubmit}>
+//           Submit
+//         </Button>
+//       </div>
+//     </Modal>
+//   );
+// }
+
+// export default CommentModal;
+
+import styles from "./CommentModal.module.css";
 import Button from "../common/Button";
-import Modal from "./Modal";
 
 function CommentModal({
   handleCloseModal,
@@ -9,20 +39,23 @@ function CommentModal({
   charCount,
 }) {
   return (
-    <Modal onClose={handleCloseModal}>
-      <div>
-        <textarea
-          value={comment}
-          onChange={handleCharChange}
-          maxLength="80"
-          placeholder="Write your comment here..."
-        />
-        <p>{charCount}/80 characters</p>
-        <Button type="secondary" onClick={handleCommentSubmit}>
+    <div>
+      <textarea
+        value={comment}
+        onChange={handleCharChange}
+        maxLength="80"
+        placeholder="Write your comment here..."
+      />
+      <p>{charCount}/80 characters</p>
+      <div className={styles.buttons}>
+        <Button type="quaternary" onClick={handleCommentSubmit}>
           Submit
         </Button>
+        <Button type="secondary" onClick={handleCloseModal}>
+          Close
+        </Button>
       </div>
-    </Modal>
+    </div>
   );
 }
 
