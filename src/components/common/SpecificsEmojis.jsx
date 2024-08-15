93fbca575126cfe85c53f59f6f99a634ef9f8a58
Gizmo <nalanbilgin@gmail.com>
2024-08-15 20:49:13 +0300

    Added tooltips to hotel item emojis

--- src/components/common/SpecificsEmojis.jsx
+++ src/components/common/SpecificsEmojis.jsx
@@ -1,7 +1,23 @@
+import styles from "./SpecificsEmojis.module.css";
+
 import { specificsToEmoji } from "../../utils/emoji.js";
 
 function SpecificsEmojis({ specifics }) {
-  return <span>{specificsToEmoji(specifics)}</span>;
+  const emojis = specificsToEmoji(specifics);
+
+  return (
+    <span>
+      {emojis.map((emoji, index) => (
+        <span
+          key={index}
+          className={styles.tooltipText}
+          title={specifics[index]} // title attribute to create tooltip
+        >
+          {emoji}
+        </span>
+      ))}
+    </span>
+  );
 }
 
 export default SpecificsEmojis;
